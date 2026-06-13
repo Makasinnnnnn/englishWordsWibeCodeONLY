04d23ba8f9b130d9862146c4ce55ed2d9af17d29
Maksim <ungamax321@gmail.com>
2026-06-13 22:45:19 +0700

    prevent stale pwa asset caching

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/sw.js",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, must-revalidate"
+          }
+        ]
+      },
+      {
+        source: "/:file(manifest.webmanifest|offline.html|reset-pwa.html)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache, must-revalidate"
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
